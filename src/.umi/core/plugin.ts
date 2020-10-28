d4086a6d9be7ad16676f0c578dba7d3c38b7aa8f
史士平 <shishiping@Mac-mini.local>
2020-10-28 16:34:21 +0800

    new

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/shishiping/WebstormProjects/antdcourse/node_modules/_@umijs_runtime@3.2.24@@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','request',],
-});
-
-export { plugin };
--- src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/shishiping/WebstormProjects/antdcourse/node_modules/_react-helmet@6.1.0@react-helmet';
